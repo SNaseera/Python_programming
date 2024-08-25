cc0b8cc6c7490737975a2dcc3cb0ea3aa6bb2731
S.Naseera Kouser <syednaseera02@gmail.com>
2024-08-25 11:37:09 +0530

    Update inheritance.py

--- inheritance.py
+++ inheritance.py
@@ -1,42 +1,42 @@
-# #single inheritance
-# class Car:
-#     color = "black"
-#     @staticmethod
-#     def start():
-#         print("car started..")
-#     @staticmethod
-#     def stop():
-#         print("car stoped..")
-# class Toyoto(Car):
-#     def __init__(self,name):
-#         self.name=name
-# c1 = Toyoto("fortuner")
-# print(c1.start())
-# print(c1.stop())
-# print(c1.name)
-# print(c1.color)
+#single inheritance
+class Car:
+    color = "black"
+    @staticmethod
+    def start():
+        print("car started..")
+    @staticmethod
+    def stop():
+        print("car stoped..")
+class Toyoto(Car):
+    def __init__(self,name):
+        self.name=name
+c1 = Toyoto("fortuner")
+print(c1.start())
+print(c1.stop())
+print(c1.name)
+print(c1.color)
 
-# #Multi-Level inheritance
-# class Car:
-#     color = "black"
-#     @staticmethod
-#     def start():
-#         print("car started..")
-#     @staticmethod
-#     def stop():
-#         print("car stoped..")
-# class Toyoto(Car):
-#     def __init__(self,brand):
-#         self.brand=brand
-# class Fortuner(Toyoto):
-#     def __init__(self,type):
-#         self.type = type
-# c1 = Toyoto("fortuner")
-# c2=Fortuner("Diesel")
-# c3=Fortuner("electric")
-# print(c1.brand)
-# print(c2.type)
-# print(c1.start())
+#Multi-Level inheritance
+class Car:
+    color = "black"
+    @staticmethod
+    def start():
+        print("car started..")
+    @staticmethod
+    def stop():
+        print("car stoped..")
+class Toyoto(Car):
+    def __init__(self,brand):
+        self.brand=brand
+class Fortuner(Toyoto):
+    def __init__(self,type):
+        self.type = type
+c1 = Toyoto("fortuner")
+c2=Fortuner("Diesel")
+c3=Fortuner("electric")
+print(c1.brand)
+print(c2.type)
+print(c1.start())
 
 #multiple inheritance
 class A:
