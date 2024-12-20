348049aed4fcfb3ce94cfb698b18076f219a4bd5
S.Naseera Kouser <syednaseera02@gmail.com>
2024-12-20 16:25:41 +0530

    Add files via upload

---
+++ bitwise operator.py
@@ -0,0 +1,18 @@
+a=4
+b=8
+print(a & b)
+
+a = 10
+b = 4
+# Print bitwise AND operation
+print("a & b =", a & b)
+
+a = 10
+b = 4
+# Print bitwise OR operation
+print("a | b =", a | b)
+
+a = 12
+b = 8
+# Print bitwise XOR operation
+print("a ^ b =", a ^ b)
